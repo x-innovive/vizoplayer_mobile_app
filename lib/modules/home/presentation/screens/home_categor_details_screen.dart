0567d966b85950b70e32b458f09ffe4d9f94e1c3
shaiful <saiful161003@gmail.com>
2025-03-21 03:21:17 +0600

    feat: category details screen designed

--- lib/modules/home/presentation/screens/home_categor_details_screen.dart
+++ lib/modules/home/presentation/screens/home_categor_details_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../../core/dummys/dummy_datas.dart';
+import '../../../../core/resources/app_values.dart';
 import '../../../../core/widgets/appbars/app_app_bar.dart';
-import '../../../../core/widgets/appbars/main_shell_app_bar.dart';
+import '../../../../core/widgets/media/movie_cover.dart';
+import '../../../../core/widgets/media_lists_views/horizontal_movie_list.dart';
+import '../../../../core/widgets/tabs/category_tab.dart';
+import '../../../../infrastructure/navigation/app_nav.dart';
+import '../../../../infrastructure/navigation/route_names.dart';
 
 class HomeCategoryDetailsScreen extends ConsumerStatefulWidget {
   const HomeCategoryDetailsScreen({super.key});
@@ -16,7 +22,69 @@ class _HomeCategoryDetailsScreenState extends ConsumerState<HomeCategoryDetailsS
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppAppBar(title: 'Movies'),
-      body: Container(),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 4),
+            Padding(
+              padding: const EdgeInsets.only(left: AppValues.paddingNormal),
+              child: CategoryTab(
+                onCategoryTap: (category) {
+                  AppNav.goRouter.push(RouteNames.homeScreen + RouteNames.homeCategoryDetailsScreen);
+                },
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: AppValues.paddingNormal,
+              ),
+              child: MovieCover(
+                assetPath: 'assets/temps/movie_covers/lordorthe.png',
+              ),
+            ),
+            const SizedBox(height: AppValues.paddingLarge),
+            HorizontalMovieList(
+              title: 'Latest Movies',
+              movieList: getMovieList(),
+              horizontalPadding: AppValues.paddingNormal,
+            ),
+            const SizedBox(height: AppValues.paddingNormal * 2),
+            HorizontalMovieList(
+              title: 'Popular Movies',
+              movieList: getMovieList(),
+              horizontalPadding: AppValues.paddingNormal,
+            ),
+            const SizedBox(height: AppValues.paddingNormal * 2),
+            HorizontalMovieList(
+              title: 'Today’s Pick for You',
+              movieList: getMovieList(),
+              horizontalPadding: AppValues.paddingNormal,
+            ),
+            const SizedBox(height: AppValues.paddingNormal * 2),
+            HorizontalMovieList(
+              title: 'Comedy Movies',
+              movieList: getMovieList(),
+              horizontalPadding: AppValues.paddingNormal,
+            ),
+            const SizedBox(height: AppValues.paddingNormal * 2),
+            HorizontalMovieList(
+              title: 'Action Movies',
+              movieList: getMovieList(),
+              horizontalPadding: AppValues.paddingNormal,
+            ),
+            const SizedBox(height: AppValues.paddingNormal * 2),
+            HorizontalMovieList(
+              title: 'Hollywood Movies',
+              movieList: getMovieList(),
+              horizontalPadding: AppValues.paddingNormal,
+            ),
+            const SizedBox(height: AppValues.paddingLarge),
+          ],
+        ),
+      ),
     );
   }
 }
